a2e664de9b8e72b46e2401602b71d9a47122591c
sushobhanjana <41752009+sushobhanjana@users.noreply.github.com>
2022-01-12 15:59:42 +0530

    firebase and google aith integrated

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo }  from '../../assets/crown.svg'
 import './header.styles.scss'
 
-export default function Header() {
+export default function Header({currentUser}) {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -17,9 +18,14 @@ export default function Header() {
                 <Link className='option' to='/contact'>
                     CONTACT
                 </Link>
-                <Link className='option' to='/login'>
-                    SIGN IN
-                </Link>
+                {currentUser 
+                    ?(<div className='option' onClick={()=>auth.signOut()}>
+                            SIGN OUT
+                    </div>)
+                    :(<Link className='option' to='/login'>
+                        SIGN IN
+                    </Link>)
+                }
             </div>
         </div>
     )
