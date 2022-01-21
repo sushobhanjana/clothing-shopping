14c44138b27a72bd7f14d3006217aad29ab6f5cb
sushobhanjana <41752009+sushobhanjana@users.noreply.github.com>
2022-01-21 15:52:36 +0530

    protect signin module when user signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route,Switch } from 'react-router-dom';
+import { Route,Switch,Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './components/header/header.component';
 import HomePage from './pages/home/home.page';
@@ -51,15 +51,22 @@ class App extends Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/contact' component={ContactPage} />
-          <Route path='/login' component={LoginregisterPage} />
+          <Route esact path='/login' render={ 
+            () => this.props.currentUser ? 
+            (<Redirect to='/' />) : (<LoginregisterPage />) 
+            } />
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App)
+export default connect(mapStateToProps,mapDispatchToProps)(App)
