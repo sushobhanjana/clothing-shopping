6d627007034b93997fd2a9b21ea75a99e8b30686
sushobhanjana <41752009+sushobhanjana@users.noreply.github.com>
2022-01-21 21:07:21 +0530

    cart icon and cart dropdown element are added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo }  from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss'
 
 const Header = ({currentUser}) => {
@@ -27,7 +29,9 @@ const Header = ({currentUser}) => {
                         SIGN IN
                     </Link>)
                 }
+                <CartIcon />
             </div>
+            <CartDropDown />
         </div>
     )
 }
